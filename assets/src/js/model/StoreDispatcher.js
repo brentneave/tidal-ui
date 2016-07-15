0a7872e2f2f32090f5c17af7db12ae6385dcaa36
Brent Neave <brent.neave@gmail.com>
2016-07-16 00:26:47 +1200

    Remove StoreDispatcher

--- assets/src/js/model/StoreDispatcher.js
+++
@@ -1,8 +0,0 @@
-const Dispatcher = require('../dispatch/Dispatcher')
-
-const StoreDispatcher = Object.freeze({
-  login: new Dispatcher(StoreDispatcher),
-  user: new Dispatcher(StoreDispatcher)
-});
-
-module.exports = StoreDispatcher;
