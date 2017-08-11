89c982a8f4ea97a038f21f997965cd3f5d3d1fd0
Brent Neave <brent.neave@gmail.com>
2017-08-11 16:00:52 +1200

    Move login call out into an action

--- assets/src/js/app/reduce.js
+++ assets/src/js/app/reduce.js
@@ -34,20 +34,16 @@ const _mutate = {
 
 
 
-    LOGIN: function(state, credentials) {
+    LOGIN: function(state, { session }) {
+        state.session = clone(session);
+        return state;
+    },
 
-        const handleResponse = function(response) {
-            console.log('handleResponse', response)
-            if (response.error) {
-                state = clone(defaultState);
-                state.errors = ['Please check your login details and try again.'];
-            } else {
-                state.session = response.session;
-            }
-            return state;
-        }
 
-        return api.login(credentials).then(handleResponse);
+
+    ERROR: function(state, { error }) {
+        // TODO
+        return state;
     },
 
 
