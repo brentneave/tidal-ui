82e19a2513a0e260797f599319be73b5083bee89
Brent Neave <brent.neave@gmail.com>
2017-08-13 06:49:13 +1200

    Add link option to section header component

--- assets/src/js/components/sectionHeader.js
+++ assets/src/js/components/sectionHeader.js
@@ -1,11 +1,25 @@
 const sectionHeader = ({ props, actions }) => ({
+
     tagName: 'div',
-    className: 'pt5 pb4 ph4 ph5-l center mw9',
-    childNodes: {
-        tagName: 'h2',
-        className: 'f3 antialiased legibility',
-        textContent: props.title || ''
-    }
+    className: 'pt5 pb4 ph4 ph5-l center mw9 flex justify-between items-center',
+    childNodes: [
+
+        {
+            tagName: 'h2',
+            className: 'f3 antialiased legibility',
+            textContent: props.title || ''
+        },
+
+        props.linkText && props.link ? {
+            tagName: 'a',
+            className: 'db f6 no-underline near-white',
+            textContent: props.linkText,
+            attributes: { href: props.link },
+            on: { click: actions.link }
+        } : null
+
+    ]
+
 });
 
 
--- assets/src/js/routes/home.js
+++ assets/src/js/routes/home.js
@@ -2,7 +2,8 @@ const
     api = require('../api/api'),
     page = require('../components/page'),
     albumList = require('../components/albumList'),
-    artistList = require('../components/artistList');
+    artistList = require('../components/artistList'),
+    sectionHeader = require('../components/sectionHeader');
 
 
 
@@ -16,22 +17,47 @@ const load = ({ state, subpath }) => ({
 const component = ({ state, props, actions }) => (
 
     page({
+
         state,
+
         props: {
+
             title: 'Hi.',
             content: {
+
                 tagName: 'div',
                 childNodes: [
+
+                    state.route.data.recommendedAlbums ? sectionHeader({
+                        props: {
+                            title: 'Recommended Albums',
+                            linkText: 'See More',
+                            link: '/recommended/albums'
+                        },
+                        actions
+                    }) : null,
+
                     state.route.data.recommendedAlbums ? albumList({
                         state: state,
                         props: { albums: state.route.data.recommendedAlbums.slice(0, 4) },
                         actions: actions
                     }) : null,
+
+                    state.route.data.recommendedArtists ? sectionHeader({
+                        props: {
+                            title: 'Recommended Artists',
+                            linkText: 'See More',
+                            link: '/recommended/artists'
+                        },
+                        actions
+                    }) : null,
+
                     state.route.data.recommendedArtists ? artistList({
                         state: state,
                         props: { artists: state.route.data.recommendedArtists.slice(0, 4) },
                         actions: actions
                     }) : null,
+
                     {
                         tagName: 'button',
                         textContent: 'Log Out',
@@ -39,10 +65,15 @@ const component = ({ state, props, actions }) => (
                             click: actions.logout
                         }
                     }
+
                 ]
+
             }
+
         },
+
         actions
+
     })
 
 )
